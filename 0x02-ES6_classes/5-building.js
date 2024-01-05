f6184e99d9fc0e3fbf3e6c4d35d7ff546f90a4e6
Rashnotech <rashnotech@gmail.com>
2024-01-05 10:45:49 +0000

    Fixed: lint error

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,19 +2,23 @@
 
 export default class Building {
   constructor (sqft) {
-    this._sqft = sqft;
+    if (this.constructor === Building) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this.sqft = sqft;
   }
 
-  set sqft (sqft) {
-    this._sqft = sqft;
+  set sqft(sqft) {
+    if (typeof sqft === 'number') {
+      this._sqft = sqft;
+    } else {
+      throw new TypeError('SQFT must be a number');
+    }
   }
 
   get sqft () {
     return this._sqft;
   }
 
-  evacuationWarningMessage () {
-     throw new Error('Class extending Building must override evacuationWarningMessage');
-
-  }
+  evacuationWarningMessage () {}
 }
---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string')
+    }
+  }
+
+  get name() {
+    return this._name
+  }
+
+  set code (code) {
+    if (typeof code === 'string') {
+      this._code = code;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
+  }
+
+  get code () {
+    return this._code;
+  }
+
+  toString() {
+    return [this.code];
+  }
+}
