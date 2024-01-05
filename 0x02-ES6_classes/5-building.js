40487ac012e3cf685eba2db4526a2ca518745bb6
Rashnotech <rashnotech@gmail.com>
2024-01-05 14:03:04 +0000

    Fixed: abstract warning

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,22 +1,14 @@
 export default class Building {
-  constructor (sqft) {
-    if (this.constructor === Building) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
     }
-    this.sqft = sqft;
+    this._sqft = sqft;
   }
 
-  set sqft(sqft) {
-    if (typeof sqft === 'number') {
-      this._sqft = sqft;
-    } else {
-      throw new TypeError('SQFT must be a number');
-    }
-  }
-
-  get sqft () {
+  get sqft() {
     return this._sqft;
   }
-
-  evacuationWarningMessage () {}
 }
