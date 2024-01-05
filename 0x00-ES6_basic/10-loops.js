1ccf0e457ec480a7c9d3a74886021519c8d9a68f
Rashnotech <rashnotech@gmail.com>
2024-01-05 13:34:23 +0000

    Fixed: Fixed lint error

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const arr = []
+  const arr = [];
 
   for (const idx of array) {
     arr.push(appendString + idx);
   }
-  
+
   return arr;
 }
