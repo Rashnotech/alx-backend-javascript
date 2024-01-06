2bc8af906b9da1994bf070dc38476f9375e79d3c
Rashnotech <rashnotech@gmail.com>
2024-01-06 17:26:08 +0100

    Fixed: validate status to assign output

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,14 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)])
-    .then((results) => [
-      ...results,
-    ]);
+  return Promise.allSettled(
+    [uploadPhoto(fileName),
+        signUpUser(firstName, lastName)
+    ])
+    .then((results) => {
+        return results.map(result => ({
+            status: result.status,
+            value: result.status === 'fulfilled' ? result.value : result.reason,
+        }));
+    });
 }
