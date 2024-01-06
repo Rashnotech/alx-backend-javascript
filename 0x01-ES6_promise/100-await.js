9919d7a026250cc5ceb2c0f7d0c3bcb1aa573ad5
Rashnotech <rashnotech@gmail.com>
2024-01-06 18:25:11 +0100

    Async function

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+    const [photo, user] = await Promise.all([
+        uploadPhoto(),
+        createUser()
+    ]);
+    
+    return {
+        photo: photo,
+        user: user
+    };
+}
