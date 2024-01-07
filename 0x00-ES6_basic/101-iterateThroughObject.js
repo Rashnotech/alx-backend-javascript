805f64831256b599aa3966467ddc0454e42c04a0
Rashnotech <rashnotech@gmail.com>
2024-01-07 12:06:08 +0100

    Iterate through object

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,11 @@
+export default function iterateThroughObject(reportWithIterator) {
+    let iteratorResult = reportWithIterator.next();
+    let output = ''
+    while (!iteratorResult.done) {
+        output += iteratorResult.value;
+        iteratorResult = reportWithIterator.next();
+        if (!iteratorResult.done)
+            output += ' | ';
+    }
+    console.log(output)
+}
