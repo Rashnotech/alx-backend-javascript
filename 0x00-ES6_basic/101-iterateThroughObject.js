0bc381285e731e09b0d0c9b7288ca829e836e555
Rashnotech <rashnotech@gmail.com>
2024-01-08 03:09:20 +0000

    Fixed: lint error

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,11 +1,10 @@
 export default function iterateThroughObject(reportWithIterator) {
-    let iteratorResult = reportWithIterator.next();
-    let output = ''
-    while (!iteratorResult.done) {
-        output += iteratorResult.value;
-        iteratorResult = reportWithIterator.next();
-        if (!iteratorResult.done)
-            output += ' | ';
-    }
-    console.log(output)
+  let iteratorResult = reportWithIterator.next();
+  let output = '';
+  while (!iteratorResult.done) {
+    output += iteratorResult.value;
+    iteratorResult = reportWithIterator.next();
+    if (!iteratorResult.done) output += ' | ';
+  }
+  console.log(output);
 }
