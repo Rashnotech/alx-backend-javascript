5f9d29c141c5b4d3b69a15cf3e4a0f81f6a34b8b
Rashnotech <rashnotech@gmail.com>
2024-01-10 01:18:26 +0000

    Weak map data structure

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,9 @@
+export const weakMap = new WeakMap();
+export const queryAPI = (endpoint) => {
+  if (!weakMap.get(endpoint)) weakMap.set(endpoint, 1);
+  else {
+    const count = weakMap.get(endpoint);
+    if (count >= 5) throw new Error('Endpoint load is high');
+    weakMap.set(endpoint, count + 1);
+  }
+};
