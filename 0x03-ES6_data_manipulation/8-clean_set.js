2399c3ff94455da497afa43caa852faad82dddd1
Rashnotech <rashnotech@gmail.com>
2024-01-10 00:51:59 +0000

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,10 @@
 export default function cleanSet(set, startString) {
-  const found = []
-  for (const set.startString);
+  let found = '';
+  if (startString === '') return found;
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      found += `${value.replace(startString, '')}-`;
+    }
+  }
+  return found.slice(0, -1);
 }
