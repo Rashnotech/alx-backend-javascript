9a284b87049bc67f9b2cf8058a871d8a013a0456
Rashnotech <rashnotech@gmail.com>
2024-01-12 09:28:44 +0000

    Fixed: throwable error

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,7 @@
 export const weakMap = new WeakMap();
 export const queryAPI = (endpoint) => {
-  if (!weakMap.get(endpoint)) weakMap.set(endpoint, 1);
-  else {
-    const count = weakMap.get(endpoint);
-    if (count >= 5) throw new Error('Endpoint load is high');
-    weakMap.set(endpoint, count + 1);
-  }
+  if (!weakMap.has(endpoint)) weakMap.set(endpoint, 0);
+  const count = weakMap.get(endpoint);
+  weakMap.set(endpoint, count + 1);
+  if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
 };
