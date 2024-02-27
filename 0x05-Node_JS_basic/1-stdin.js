90e5d359d7605181ff8e287788341dbbc335b3e0
Rashnotech <rashnotech@gmail.com>
2024-02-27 22:17:40 +0000

    Fixed: bash header

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,3 @@
-#!/usr/bin/env node
-/**
- * a program that execute command line
- */
-
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('readable', function () {
