c7d12a3cce57155d65f7940d484787ba59d31ae9
Rashnotech <rashnotech@gmail.com>
2024-02-27 22:20:51 +0000

    Fixed: Using process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,4 +1,4 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', function () {
   const name = process.stdin.read();
@@ -6,5 +6,5 @@ process.stdin.on('readable', function () {
 });
 
 process.stdin.on('end', function () {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
