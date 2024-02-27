23fc383b191491865c3e0fe8cf4c0c77f462308d
Rashnotech <rashnotech@gmail.com>
2024-02-27 22:06:16 +0000

    Fixed: syntax

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -3,9 +3,7 @@
  * displayMessage - a function that display message
  * params: a string literal passed to be printed
  */
-
 const displayMessage = (params) => {
   console.log(params);
-}
-
+};
 module.exports = displayMessage;
