103e18f17099b4202331518bb16ea07f6f4c0740
Rashnotech <rashnotech@gmail.com>
2024-03-10 16:22:11 +0000

    Fixed: add done method

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -3,10 +3,15 @@ const request = require('request')
 
 
 describe('index', () => {
-  it('test index', () => {
+  it('test index', (done) => {
     request.get('http://localhost:7865', (err, res, body) => {
+      if (err) {
+	done(err);
+        return
+      }
       expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Welcome to the payment system')
+      done();
     })
   })
 })
