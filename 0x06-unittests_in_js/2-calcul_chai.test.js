1769e28ea156838a666401b8b4644b4ac9d90da4
Rashnotech <rashnotech@gmail.com>
2024-03-10 10:57:39 +0000

    Seperate test

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -3,14 +3,17 @@ const calculateNumber = require('./1-calcul.js')
 
 
 describe('calculateNumber', () => {
-  describe('TestCases', () => {
+  describe('Test Sum', () => {
     it('should return 6', () => {
       expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6)
     })
+  })
+  describe('Test Substract', () => {
     it('should return -4', () => {
       expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4)
     })
-
+  })
+  describe('Test Division', () => {
     it('should return 0.2', () => {
       expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2)
     })
