a88731753c178fadd78da2da231676d9a0b585af
Rashnotech <rashnotech@gmail.com>
2024-03-10 16:49:53 +0000

    Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,39 @@
+const { expect } = require('chai')
+const request = require('request')
+
+
+describe('index', () => {
+  it('test index', (done) => {
+    request.get('http://localhost:7865', (err, res, body) => {
+      if (err) {
+	done(err);
+        return
+      }
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system')
+      done();
+    })
+  })
+
+  it('test /cart/:id/(//d+)', () => {
+    request.get('http://localhost:7865/cart/12', (err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 12')
+    })
+  })
+
+  it('test /cart/:id/(//d+)', () => {
+    request.get('http://localhost:7865/cart/hello', (err, res, body) => {
+      expect(res.statusCode).to.be.equal(404);
+    })
+  })
+
+  it('test /cart/:id/(//d+)', () => {
+    request.get('http://localhost:7865/cart/abc123!', (err, res, body) => {
+      expect(res.statusCode).to.be.equal(404);
+    })
+  })
+
+})
+
+
